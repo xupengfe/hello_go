a2b185ef048e03f4d41c8f4098af5c859cfcb2b4
Pengfei Xu <pengfei.xu@intel.com>
2023-07-19 15:43:01 +0800

    func.go: add the function test
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

---
+++ basic/func.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// 定义 Write函数 返回值有两个，一个为name，一个age为
+func Write() (name string, age int) {
+	return "Dong", 9
+}
+
+// 定义 Read函数
+func Read(name string, age int) {
+	fmt.Println(name, "已经", age, "岁了")
+}
+
+func main() {
+	Read(Write()) // 迈莫coding  已经  1  岁
+}
