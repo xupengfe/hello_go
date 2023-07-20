45d387091f753e81a8d3ad9c7662aac409a32e92
Pengfei Xu <pengfei.xu@intel.com>
2023-07-20 14:42:25 +0800

    var2.go: add more var
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- basic/var2.go
+++ basic/var2.go
@@ -5,6 +5,9 @@ import "fmt"
 var i, j int = 1, 2
 
 func main() {
-	var c, python, java = true, false, "no!"
-	fmt.Println(i, j, c, python, java)
+	var py, ja = true, false
+	k := 1.1
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, c, k, py, ja, python, java)
 }
